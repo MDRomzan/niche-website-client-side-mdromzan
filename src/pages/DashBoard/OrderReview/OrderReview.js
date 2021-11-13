e79287b0d2ad3babf3153e86d4cd08c38dc034e0
MDRomzan <mdromjan483@gmail.com>
2021-11-13 14:00:32 +0600

    asd

--- src/pages/DashBoard/OrderReview/OrderReview.js
+++ src/pages/DashBoard/OrderReview/OrderReview.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import useAuth from '../../../Hooks/useAuth';
-
+import { useForm } from 'react-hook-form';
+import { clearTheCart, getStoredCart } from '../../../Uitilities/fakedb';
 const OrderReview = () => {
+    const {handleSubmit,register,reset,formState: { errors }}=useForm();
     const {user,token}=useAuth();
     const [orders,setOrdersConfirm]=useState([]);
   // console.log(order)
@@ -17,10 +19,50 @@ const OrderReview = () => {
         
         
     },[]);
+
+
+    const onSubmit=(data)=>{
+        const savedCart=getStoredCart()
+        data.order =savedCart;
+    //   console.log(data)
+        fetch('https://mysterious-cliffs-16987.herokuapp.com/orders', {
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+        .then(res =>res.json())
+        .then(result =>{
+            // console.log(result);
+            if(result.insertedId){
+                alert("Booking Successfully Done")
+                clearTheCart()
+                reset()
+            }
+        })
+
+    };
+    
     return (
         <div>
             <h1>This  is Order Review:{orders.length}</h1>
+             <form className="shipping-form" onSubmit={handleSubmit(onSubmit)}>
 
+                <input defaultValue={user.displayName} {...register("name")} />
+
+                <input defaultValue={user.email} {...register("email", { required: true })} />
+                
+
+                
+                <input type="number" {...register("price")} placeholder="Please Review" />
+
+                <br />
+
+
+
+                <input type="submit" />
+            </form> 
 
         </div>
     );
