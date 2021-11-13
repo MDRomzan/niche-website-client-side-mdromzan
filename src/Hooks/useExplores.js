12e82ba244d622b697ec9b6506c2135df1775962
MDRomzan <mdromjan483@gmail.com>
2021-11-13 21:50:13 +0600

    done

--- src/Hooks/useExplores.js
+++ src/Hooks/useExplores.js
@@ -1,15 +1,16 @@
+import { useEffect, useState } from "react";
 
 
-import {
-    useState
-} from "react"
+
 
 const useExplore = () => {
     const [explores, setExplores] = useState([]);
 
-    fetch("https://sheltered-mountain-47444.herokuapp.com/explores")
+   useEffect(()=>{
+        fetch("https://sheltered-mountain-47444.herokuapp.com/explores")
         .then(res => res.json())
         .then(data => setExplores(data));
+   },[])
     return {
         explores,
         setExplores
